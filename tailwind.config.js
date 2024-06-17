afc5c59bcfa83cf0f4f063064e73545e5f80037a
abolfazl <abolfazl>
2024-06-17 09:32:33 +0330

    initialize

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        quiz: {
+          black: { 
+            100: "#1C1C1C",
+            200: "#1C1C1E",
+           },
+          gray: {
+            100: "#8B8B8B",
+            200: "#979799",
+            300: "#3B3B3D",
+            400: "#3D3D3F",
+            500: "#828282",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
